f74fc59f21a6dffd7ad523a0bf5011b6eb94ce01
mcnestrb <brianmcnestry@gmail.com>
2017-03-14 11:26:29 +0000

    basic understanding of global state

--- CPS.py
+++ CPS.py
@@ -1,14 +1,42 @@
-from twisted.internet import reactor, protocol
+import sys
+from twisted.python import log
+from twisted.internet import reactor
+from twisted.internet.protocol import ServerFactory, ClientFactory, Protocol
 
 PORT = 9000
 
-class CPS(protocol.Protocol):
-    pass
+class CPS(Protocol):
+    def __init__(self, factory):
+        self.factory = factory
 
-class CPSFactory(protocol.Factory):
-    protocol = CPS
+    def connectionMade(self):
+        log.msg('Client connection from {}'.format(self.transport.getPeer()))
 
-print("Running Server")
+    def dataReceived(self, data):
+        log.msg('Data received {}'.format(data))
+        if( self.factory.state == 'first'):
+            log.msg('Reply as normal')
+            self.factory.state = 'second'
+            log.msg('{}'.format(self.factory.state))
+            self.transport.write(data)
+        elif ( self.factory.state == 'second'):
+            log.msg('Refuse connection')
+            self.factory.state = 'first'
+            log.msg('{}'.format(self.factory.state))
+            self.transport.write('close'.encode())
+
+    def connectionLost(self, reason):
+        log.msg('Lost connection because {}'.format(reason))
+
+class CPSFactory(ServerFactory):
+    def __init__(self):
+        self.state = 'first'
+
+    def buildProtocol(self, addr):
+        return CPS(self)
+
+log.startLogging(sys.stdout)
+log.msg("Running Server")
 factory = CPSFactory()
 reactor.listenTCP(PORT, factory)
 reactor.run()
--- EC.py
+++ EC.py
@@ -1,16 +1,52 @@
-from twisted.internet import reactor, protocol
+import sys
+from twisted.python import log
+from twisted.internet import reactor
+from twisted.internet.protocol import ClientFactory, Protocol
 
 HOST = 'localhost'
 PORT = 9000
 
-class EC(protocol.Protocol):
+class EC(Protocol):
     def connectionMade(self):
-        print("connected!")
+        data = input('\nType a message to send to the CPS\n')
+        if (data == 'q'):
+            self.transport.loseConnection
+        else:
+            self.transport.write(data.encode())
+            log.msg('Data sent {}'.format(data))
 
-class ECFactory(protocol.ClientFactory):
-    protocol = EC
+    def dataReceived(self, data):
+        log.msg('Data received {}'.format(data))
+        if( data == b'close'):
+            log.msg('Closing')
+            self.transport.loseConnection()
+        else:
+            data = input('\nType a message to send to the CPS\n')
+            if (data == 'q'):
+                self.transport.loseConnection
+            else:
+                self.transport.write(data.encode())
+                log.msg('Data sent {}'.format(data))
 
-print("Running EC")
+    def connectionLost(self, reason):
+        log.msg('Lost connection because {}'.format(reason))
+
+class ECFactory(ClientFactory):
+    def startedConnecting(self, connector):
+        log.msg('Started to connect...')
+
+    def buildProtocol(self, addr):
+        log.msg('Connected')
+        return EC()
+
+    def ECConnectionLost(self, connector, reason):
+        log.msg('Lost connection because: {}'.format(reason))
+
+    def ECConnectionFailed(self, connector, reason):
+        log.msg('Connection failed because {}'.format(reason))
+
+log.startLogging(sys.stdout)
+log.msg("Running Client")
 factory = ECFactory()
 reactor.connectTCP(HOST, PORT, factory)
 
