68e266a1098d66dea516d2ecf8db3f24fdb2f603
mcnestrb <brianmcnestry@gmail.com>
2017-03-18 18:36:11 +0000

    make basic FSM and proper idle state

--- EC/ECFSM.py
+++ EC/ECFSM.py
@@ -1,21 +1,39 @@
-class CPSFSM():
+from twisted.python import log
+from EnumEC import ECState as state
+
+class ECFSM():
     def __init__(self, protocol):
         self.protocol = protocol
-        self.factory = protocol.factory
 
-    def idleState():
+    def idleState(energy):
+        if (energy < 0):
+            log.msg('Moving from IDLE to DEMAND')
+            self.protocol.factory.state = state.DEMAND
+            self.protocol.transport.write(energy.encode())
+        elif (energy > 0):
+            log.msg('Moving from IDLE to SUPPLY')
+            self.protocol.factory.state = state.SUPPLY
+            self.protocol.transport.write(energy.encode())
+        else:
+            log.msg('Remain in IDLE state')
 
 
     def demandState():
-
+        log.msg('Moving from DEMAND to RECEIVE')
+        self.protocol.factory.state = state.RECEIVE
 
     def supplyState():
-
+        log.msg('Moving from SUPPLY to EST_1')
+        self.protocol.factory.state = state.EST_1
 
     def est1State():
-
+        log.msg('Moving from EST_1 to EST_2')
+        self.protocol.factory.state = state.EST_2
 
     def est2State():
-
+        log.msg('Moving from EST_2 to IDLE')
+        self.protocol.factory.state = state.IDLE
 
     def receiveState():
+        log.msg('Moving from RECEIVE to IDLE')
+        self.protocol.factory.state = state.IDLE
--- EC/ECProtocol.py
+++ EC/ECProtocol.py
@@ -11,14 +11,19 @@ class ECProtocol(Protocol):
 
     def connectionMade(self):
         self.factory.state = state.IDLE
+        self.FSM.idleState(self.factory.energy)
 
     def dataReceived(self, data):
-        if (self.factory.state == state.IDLE):
         elif (self.factory.state == state.DEMAND):
+            self.FSM.demandState()
         elif (self.factory.state == state.SUPPLY):
+            self.FSM.supplyState()
         elif (self.factory.state == state.EST_1):
+            self.FSM.est1State()
         elif (self.factory.state == state.EST_2):
+            self.FSM.est2State()
         elif (self.factory.state == state.RECEIVE):
+            self.FSM.receiveState()
 
     def connectionLost(self, reason):
         log.msg('Lost connection because {}'.format(reason))
