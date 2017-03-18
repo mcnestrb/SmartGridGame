33b33692b7e795bfa994459de7a9fde7671f77c7
mcnestrb <brianmcnestry@gmail.com>
2017-03-18 18:35:03 +0000

    make CPS idle state

--- CPS/CPSFSM.py
+++ CPS/CPSFSM.py
@@ -1,18 +1,32 @@
+from twisted.python import log
+from EnumCPS import CPSState as state
+
 class CPSFSM():
     def __init__(self, protocol):
         self.protocol = protocol
-        self.factory = protocol.factory
 
-    def idleState():
-
+    def idleState(data, peer):
+        energy = int(data)
+        if (peer in self.protocol.factory.ECs):
+            if ( energy > 0 ):
+                self.protocol.factory.suppliers.update({peer: energy})
+            elif: (energy < 0):
+                self.protocol.factory.bidders.update({peer: energy})
+        else:
+            log.msg('Error peer not connected')
 
     def startState():
-
+        log.msg('Moving from START to INIT')
+        self.protocol.factory.state = state.IDLE
 
     def initState():
-
+        log.msg('Moving from INIT to OPT')
+        self.protocol.factory.state = state.OPT
 
     def optState():
-
+        log.msg('Moving from OPT to DISTRIBUTE')
+        self.protocol.factory.state = state.DISTRIBUTE
 
     def distributeState():
+        log.msg('Moving from DISTRIBUTE to IDLE')
+        self.protocol.factory.state = state.IDLE
--- CPS/CPSProtocol.py
+++ CPS/CPSProtocol.py
@@ -16,12 +16,17 @@ class CPSProtocol(Protocol):
         log.msg('All connection {}'.format(self.factory.ECs))
 
     def dataReceived(self, data):
+        peer = self.transport.getPeer()
         if (self.factory.state == state.IDLE):
-            self.FSM.
+            self.FSM.idleState(data, peer)
         elif (self.factory.state == state.START):
+            self.FSM.startState()
         elif (self.factory.state == state.INIT):
+            self.FSM.initState()
         elif (self.factory.state == state.OPT):
+            self.FSM.optState()
         elif (self.factory.state == state.DISTRIBUTE):
+            self.FSM.distributeState()
 
     def connectionLost(self, reason):
         log.msg('Lost connection because {}'.format(reason))
